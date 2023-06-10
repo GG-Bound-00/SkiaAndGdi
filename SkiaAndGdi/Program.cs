d6367009516c2d379ee99bf1d0221dfa5eaa018b
Colin Wang <Colin.Wang.Developer@outlook.com>
2023-06-11 03:17:30 +0800

    修改格式

--- SkiaAndGdi/Program.cs
+++ SkiaAndGdi/Program.cs
@@ -7,13 +7,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
 	internal class SkiaAndGdi
 	{
 
-	/*
-		Skia 与 Gdi 这两个渲染方式与文档中给出的数值均会存在一些偏差，需要手动微调。
-		但是Skia与Gdi相比较还是具有一定优势的。
-		Skia对字符串宽度测量更为精准，并且Skia使用文档中给定的XY轴坐标绘制出来的文字
-		几乎没有偏差。
-		但是Skia和Gdi一样，它不能控制文字之间的间距，需要手动绘制。
-	 */
+		/*
+			Skia 与 Gdi 这两个渲染方式与文档中给出的数值均会存在一些偏差，需要手动微调。
+			但是Skia与Gdi相比较还是具有一定优势的。
+			Skia对字符串宽度测量更为精准，并且Skia使用文档中给定的XY轴坐标绘制出来的文字
+			几乎没有偏差。
+			但是Skia和Gdi一样，它不能控制文字之间的间距，需要手动绘制。
+		 */
 
 		static void Main(string[] args)
 		{
